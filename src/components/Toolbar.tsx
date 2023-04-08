2b0b386fa26fc13b1fc64b3faf44c35b01b755bc
Kat Berge <kat.n.berge@gmail.com>
2023-04-08 12:15:18 -0700

    Adds popup for API key

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Toolbar.css'
 
 const Toolbar = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Do something with the input value, e.g. submit to server or update state
+    console.log(inputValue);
+    setInputValue('');
+    setIsPopupOpen(false);
+  };
+
   return (
     <div id="toolbar">
-        <button id="api-button">API Key</button>
+        <button onClick={togglePopup} id="api-button">API Key</button>
+        {isPopupOpen && (
+          <div>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Enter API Key..."
+              />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        )}
         <h1 id="app-name">GISTAR</h1>
         <button id="info-button">Info</button>
     </div>
