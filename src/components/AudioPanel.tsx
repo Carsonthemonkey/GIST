a6629c479108085c5c66e39f63c5597fd8365bc9
maxdecorso <lc21-0542@lclark.edu>
2023-04-08 16:02:44 -0700

    Added Icons
    Added icons to the API Key, Info Button, and Audio Panel.

--- src/components/AudioPanel.tsx
+++ src/components/AudioPanel.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../styles/AudioPanel.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
 
 const AudioPanel = () => {
   return (
     <div id="audio-panel">
-      <p>AudioPanel</p>
+      <FontAwesomeIcon icon={faMicrophone} size="2x" />
     </div>
   )
 }
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
 import '../styles/Toolbar.css'
 import APIPopUp from './APIPopUp';
+//import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 
 const Toolbar = () => {
+  
+  
   const [isPopUpOpen, setIsPopUpOpen] = useState(false);
 
   const togglePopUp = () => {
@@ -12,9 +18,9 @@ const Toolbar = () => {
   return (
     <div>
       <div id="toolbar">
-          <button onClick={togglePopUp} id="api-button">API Key</button>
+          <button onClick={togglePopUp} id="api-button"><FontAwesomeIcon icon={faKey}size='2x' /></button>
           <h1 id="app-name">GISTAR</h1>
-          <button id="info-button">Info</button>
+          <button id="info-button"><FontAwesomeIcon icon={faCircleInfo}size='2x' /></button>
       </div>
       {isPopUpOpen && (
             <APIPopUp isPopUpOpen={isPopUpOpen} togglePopUp={togglePopUp} />
