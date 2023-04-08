774d15255ea393a85e9040e6649cb1bcdfd2457e
Kat Berge <kat.n.berge@gmail.com>
2023-04-08 13:53:52 -0700

    Adds elements to summary panel

--- src/components/SummaryPanel.tsx
+++ src/components/SummaryPanel.tsx
@@ -3,7 +3,11 @@ import '../styles/SummaryPanel.css'
 
 const SummaryPanel = () => {
   return (
-    <div id="summary-panel">SummaryPanel</div>
+    <div id="summary-panel">
+      <h2 id="summary-title">Summarize</h2>
+      <button id="notes-button">Generate Notes</button>
+      <p id="summary-content">This is where the summary will go</p>
+    </div>
   )
 }
 
