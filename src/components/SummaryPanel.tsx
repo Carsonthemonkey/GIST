d8918d6eae2a6556051bd4b632ae0d6e772b0f09
Kat Berge <kat.n.berge@gmail.com>
2023-04-08 17:20:22 -0700

    Adds summary drop down button

--- src/components/SummaryPanel.tsx
+++ src/components/SummaryPanel.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/SummaryPanel.css'
 
 interface Props {
@@ -7,10 +7,23 @@ interface Props {
 }
 
 const SummaryPanel = (props: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div id="summary-panel">
       <h2 id="summary-title">Summarize</h2>
       <button id="notes-button">Generate Notes</button>
+      <button id="summary-drop-down-button" onClick={toggleDropdown}>v</button>
+      {isOpen && (
+        <ul>
+          <li>Bullets</li>
+          <li>Summary</li>
+        </ul>
+      )}
       <p id="summary-content">This is where the summary will go <br/>
          Cras non tortor turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus aliquet ante turpis, eget dapibus nisl malesuada vulputate. Sed ut neque magna. Pellentesque sagittis tortor eu vulputate gravida. Morbi nec luctus metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin finibus, dui in porttitor faucibus, est erat blandit odio, et viverra sapien dui at mi. Integer volutpat orci vitae risus cursus rutrum. Mauris et tristique nunc, vitae fermentum dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris quis tellus feugiat, suscipit elit in, rhoncus purus. Fusce dictum pharetra diam, sed vestibulum nunc mollis ac. Aenean placerat orci at est placerat, nec tempus elit auctor.
         Aenean at ex et lacus dignissim varius. Phasellus interdum libero leo, sed imperdiet orci molestie fringilla. Sed laoreet neque non pulvinar facilisis. Pellentesque in sapien consequat, semper leo et, ullamcorper velit. Aliquam ut tristique justo. Mauris finibus, lectus vel consectetur tincidunt, orci neque dignissim elit, et maximus metus ligula non purus. Duis risus odio, hendrerit non semper ac, euismod nec risus. Aliquam vehicula, eros vel cursus porta, urna eros volutpat arcu, eu auctor felis libero vel erat. Integer sed orci elementum, vestibulum nunc vel, pharetra lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam eget erat ut nulla dictum imperdiet id ac nisl. Quisque eleifend auctor velit, sed finibus quam blandit finibus. Donec vehicula nibh a vestibulum scelerisque. Integer luctus laoreet libero, sit amet bibendum erat interdum quis. Curabitur id lectus vitae ante suscipit cursus.
