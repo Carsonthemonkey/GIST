558a3e5519fc963bf601459cba96d26f6406db38
Carson Reader <carsonthemonkey@gmail.com>
2023-04-08 22:01:51 -0700

    File drop now only accepts valid audio file types

--- src/components/TranscriptPanel.tsx
+++ src/components/TranscriptPanel.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/TranscriptPanel.css";
+import Modal from "react-modal";
 
 interface Props {
     APIKeyProp: string;
@@ -9,6 +10,7 @@ interface Props {
 const TranscriptPanel = (props: Props) => {
     const [fileUploaded, setFileUploaded] = React.useState(false);
     const [audioFile, setAudioFile] = React.useState<File | null>(null);
+    const [modalIsOpen, setModalIsOpen] = React.useState(false);
 
     const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
         //TODO highlight the screen in some way so it is clear that a file is being dragged over the screen
@@ -16,9 +18,16 @@ const TranscriptPanel = (props: Props) => {
     };
 
     const handleFileDrop = (event: React.DragEvent<HTMLDivElement>) => {
-        event.preventDefault();
-        const file = event.dataTransfer.files[0];
-        setAudioFile(file);
+      event.preventDefault();
+      const file = event.dataTransfer.files[0];
+      const validFileTypes = ["mp3", "mp4", "mpeg", "mpga", "m4a", "wav", "webm"];
+      if(!validFileTypes.includes(file.name.split(".").pop()!)){
+          setModalIsOpen(true);
+          return;
+      }
+      setAudioFile(file);
+        // console log the file extension
+        console.log(file.name.split(".").pop());
         //TODO Check if file is compatible file type
         setFileUploaded(true);
     };
@@ -29,6 +38,7 @@ const TranscriptPanel = (props: Props) => {
             onDrop={handleFileDrop}
             onDragOver={handleDragOver}
         >
+          <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>File is not a valid filetype. Please use mp3, mp4, mpeg, mpga, m4a, wav, or webm.</Modal>
             <h2 id="transcript-title">Transcript</h2>
             {/* TODO make this button more in the style of the generate notes button*/}
             <button>Transcribe</button>
