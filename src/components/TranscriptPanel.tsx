fbe5072c1fba9484022838d7bddba85bed0b2261
Carson Reader <carsonthemonkey@gmail.com>
2023-04-08 21:17:47 -0700

    Added file drag and drop to transcrupt panel

--- src/components/TranscriptPanel.tsx
+++ src/components/TranscriptPanel.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
-import '../styles/TranscriptPanel.css'
+import React from "react";
+import "../styles/TranscriptPanel.css";
 
 interface Props {
-  APIKeyProp: string;
-  transcriptProp: string;
+    APIKeyProp: string;
+    transcriptProp: string;
 }
 
 const TranscriptPanel = (props: Props) => {
-  return (
-    <div id="transcript-panel">
-      <h2 id="transcript-title">Transcript</h2>
-      <p id="transcript-content">This is where the transcript will go <br/>
-        {props.transcriptProp}
-      </p>
-    </div>
-  )
-}
+    const [fileUploaded, setFileUploaded] = React.useState(false);
+    const [audioFile, setAudioFile] = React.useState<File | null>(null);
 
-export default TranscriptPanel
+    const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+        //TODO highlight the screen in some way so it is clear that a file is being dragged over the screen
+        event.preventDefault();
+    };
+
+    const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        const file = event.dataTransfer.files[0];
+        setAudioFile(file);
+        //TODO Check if file is compatible file type
+        setFileUploaded(true);
+    };
+    return (
+        <div
+            id="transcript-panel"
+            className="dropzone"
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+        >
+            <h2 id="transcript-title">Transcript</h2>
+            <p id="transcript-content">
+                <br />
+                {/* Add a nice style to this */}
+                {!fileUploaded && "Drag and drop your audio file here"}
+                {fileUploaded && `fileName: ${audioFile?.name}`}
+                {fileUploaded && props.transcriptProp}
+            </p>
+        </div>
+    );
+};
+
+export default TranscriptPanel;
