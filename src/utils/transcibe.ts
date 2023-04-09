93977e993958e8de3bdb5a6104108e44caf578a8
Carson Reader <carsonthemonkey@gmail.com>
2023-04-09 01:02:19 -0700

    Added debug option to transcribe script for testing without an API key

--- src/utils/transcibe.ts
+++ src/utils/transcibe.ts
@@ -1,9 +1,68 @@
 //This function can only transcibe, not translate. Translation is a different API call.
 export default async function transcribeWhisper(
+    debug: boolean,
     audioFile: File,
     language: string,
     API_KEY: string
 ) {
+    if (debug) {
+        //wait 2 seconds to simulate a network call
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        return `We're no strangers to love
+        You know the rules and so do I (do I)
+        A full commitment's what I'm thinking of
+        You wouldn't get this from any other guy
+        I just wanna tell you how I'm feeling
+        Gotta make you understand
+        Never gonna give you up
+        Never gonna let you down
+        Never gonna run around and desert you
+        Never gonna make you cry
+        Never gonna say goodbye
+        Never gonna tell a lie and hurt you
+        We've known each other for so long
+        Your heart's been aching, but you're too shy to say it (say it)
+        Inside, we both know what's been going on (going on)
+        We know the game and we're gonna play it
+        And if you ask me how I'm feeling
+        Don't tell me you're too blind to see
+        Never gonna give you up
+        Never gonna let you down
+        Never gonna run around and desert you
+        Never gonna make you cry
+        Never gonna say goodbye
+        Never gonna tell a lie and hurt you
+        Never gonna give you up
+        Never gonna let you down
+        Never gonna run around and desert you
+        Never gonna make you cry
+        Never gonna say goodbye
+        Never gonna tell a lie and hurt you
+        We've known each other for so long
+        Your heart's been aching, but you're too shy to say it (to say it)
+        Inside, we both know what's been going on (going on)
+        We know the game and we're gonna play it
+        I just wanna tell you how I'm feeling
+        Gotta make you understand
+        Never gonna give you up
+        Never gonna let you down
+        Never gonna run around and desert you
+        Never gonna make you cry
+        Never gonna say goodbye
+        Never gonna tell a lie and hurt you
+        Never gonna give you up
+        Never gonna let you down
+        Never gonna run around and desert you
+        Never gonna make you cry
+        Never gonna say goodbye
+        Never gonna tell a lie and hurt you
+        Never gonna give you up
+        Never gonna let you down
+        Never gonna run around and desert you
+        Never gonna make you cry
+        Never gonna say goodbye
+        Never gonna tell a lie and hurt you`;
+    }
     console.log("transcribing with transcribeWhisperForm");
     const data = new FormData();
     data.append("file", audioFile);
