5b5659251bde4492830c1df286bf66cb289d1880
Kat Berge <kat.n.berge@gmail.com>
2023-04-09 12:12:36 -0700

    Removes audio panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,34 @@
-import React, { useState, createContext } from 'react'
-import './styles/global.css'
-import './styles/App.css'
-import Toolbar from './components/Toolbar'
-import TranscriptPanel from './components/TranscriptPanel'
-import SummaryPanel from './components/SummaryPanel'
-import AudioPanel from './components/AudioPanel'
+import React, { useState, createContext } from "react";
+import "./styles/global.css";
+import "./styles/App.css";
+import Toolbar from "./components/Toolbar";
+import TranscriptPanel from "./components/TranscriptPanel";
+import SummaryPanel from "./components/SummaryPanel";
+import AudioPanel from "./components/AudioPanel";
 
-export const Context = createContext<[string, React.Dispatch<React.SetStateAction<string>>]>(['', () => {}]);
+export const Context = createContext<
+    [string, React.Dispatch<React.SetStateAction<string>>]
+>(["", () => {}]);
 
 function App() {
-  const [APIKey, setAPIKey] = useState('');
+    const [APIKey, setAPIKey] = useState("");
 
-  //This will need to be changed later
-  const [transcript, setTranscriptText] = useState(``);
+    //This will need to be changed later
+    const [transcript, setTranscriptText] = useState(``);
 
-  return (
-    <Context.Provider value={[APIKey, setAPIKey]}>
-      <div className="App">
-        <Toolbar />
-        <TranscriptPanel 
-          APIKeyProp={APIKey}
-          transcriptProp={transcript}
-          setTranscriptProp={setTranscriptText}
-          />
-        <SummaryPanel 
-          APIKeyProp={APIKey}
-          transcriptProp={transcript}
-          />
-        <AudioPanel />
-      </div>
-    </Context.Provider>
-  )
+    return (
+        <Context.Provider value={[APIKey, setAPIKey]}>
+            <div className="App">
+                <Toolbar />
+                <TranscriptPanel
+                    APIKeyProp={APIKey}
+                    transcriptProp={transcript}
+                    setTranscriptProp={setTranscriptText}
+                />
+                <SummaryPanel APIKeyProp={APIKey} transcriptProp={transcript} />
+            </div>
+        </Context.Provider>
+    );
 }
 
-export default App
+export default App;
