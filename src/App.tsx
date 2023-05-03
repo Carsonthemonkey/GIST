92122b74e12654672fc0eb6776c1b4e4388d6cb3
Carson Reader <carsonthemonkey@gmail.com>
2023-05-03 11:33:35 -0700

    Added Dark mode toggle script and keyboard shortcut

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import "./styles/global.css";
 import "./styles/App.css";
 import Toolbar from "./components/Toolbar";
 import TranscriptPanel from "./components/TranscriptPanel";
 import SummaryPanel from "./components/SummaryPanel";
+import { setColorScheme } from "./utils/colorSchemeChanger";
 
 export const Context = createContext<
     [string, React.Dispatch<React.SetStateAction<string>>]
@@ -11,9 +12,30 @@ export const Context = createContext<
 
 function App() {
     const [APIKey, setAPIKey] = useState("");
-
+    let colorTheme = "light";
     //This will need to be changed later
     const [transcript, setTranscriptText] = useState(``);
+
+    useEffect(() => {
+        //handles keyboard shortcuts
+        document.addEventListener("keydown", handleKeyDown);
+    }, []);
+
+    function handleKeyDown(event: KeyboardEvent) {
+        if (event.ctrlKey && event.key === "l") {
+            event.preventDefault();
+            if(colorTheme === "dark"){
+                console.log(colorTheme)
+                setColorScheme("light");
+                colorTheme = "light";
+            }
+            else if (colorTheme === "light"){
+                setColorScheme("dark");
+                colorTheme = "dark";
+                console.log(colorTheme)
+            }
+        }
+    }
 
     return (
         <Context.Provider value={[APIKey, setAPIKey]}>
