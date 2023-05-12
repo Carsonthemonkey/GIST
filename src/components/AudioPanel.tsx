9d89919041be76e630f75b6d8cef65d7049954ba
Carson Reader <carsonthemonkey@gmail.com>
2023-05-12 11:58:28 -0700

    Fixes issue where playhead would be for previous file

--- src/components/AudioPanel.tsx
+++ src/components/AudioPanel.tsx
@@ -31,7 +31,7 @@ const AudioPanel = ({ audioFile, fileIsUploaded }: AudioPanelProps) => {
             }
             audioObject.addEventListener("timeupdate", (event) => {
                 setCurrentTime(audioObject.currentTime);
-                setPlayheadPosition((audioObject.currentTime / audioDuration) * 100);
+                setPlayheadPosition((audioObject.currentTime / audioObject.duration) * 100);
             })
         }
         else{
@@ -62,8 +62,6 @@ const AudioPanel = ({ audioFile, fileIsUploaded }: AudioPanelProps) => {
 
     return (
         <div id="audio-panel">
-            {/*//! This appears to be a load-bearing button. When I remove it, the playhead no longer works for some reason ¯\_(ツ)_/¯ Ok It doesnt even work when I set display none?? like it has to be there for some reason??? This may be a weird CSS problem with the playhead or something. Aaaand it is also really buggy in electron. Oh boy.
-            //TODO: FIX THIS */}
 
             <button className="icon">
                 <FontAwesomeIcon icon={faMicrophone} size="2x" />
