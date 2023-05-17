caa933479b46aca07bdc0a66e0bee0a4d8bd98ef
Carson Reader <carsonthemonkey@gmail.com>
2023-05-16 20:18:46 -0700

    Adds python transcription file
    Transcription can be done locally using this script. Next step is to implment compiling automations, and a js script that calls the compiled python executable.

---
+++ src/utils/python/Local whisper notes.md
@@ -0,0 +1,7 @@
+
+Whisper [Documentation](https://github.com/openai/whisper)
+[Thread on translation](https://github.com/openai/whisper/discussions/1309)
+
+- `pip install -U openai-whisper`
+- `pip install pyinstaller`
+
---
+++ src/utils/python/local_whisper.py
@@ -0,0 +1,18 @@
+import whisper
+import sys
+
+args = sys.argv[1:]
+audio_path = args[0]
+
+if len(args) > 1:
+    mode = args[1]
+else:
+    mode = "transcribe"
+
+model = whisper.load_model("base")
+audio = whisper.load_audio(audio_path)
+
+# This will need to include translation via a command line arg
+result = model.transcribe(audio, task=mode)
+
+print(result["segments"])
