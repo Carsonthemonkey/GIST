6d60e3f60b60ef123c6a191293990096d54ede55
Kat Berge <kat.n.berge@gmail.com>
2023-06-04 13:10:41 -0700

    More comment editing

--- src/components/SummaryPanel/SummaryPanel.tsx
+++ src/components/SummaryPanel/SummaryPanel.tsx
@@ -1,8 +1,5 @@
 import React, { useState, useContext } from "react";
 import "./SummaryPanel.css";
-// import summarizeGPT from "../../utils/summarize";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import Dropdown from "../Dropdown/Dropdown";
 import PanelAnchor from "../PanelAnchor/PanelAnchor";
 import promptsOBJ from "../../assets/prompts.json";
@@ -10,7 +7,7 @@ import MarkdownFormatter from "../MarkdownFormatter/MarkdownFormatter";
 // import { Context } from "../../App";
 
 interface Props {
-    // might want to use context for these since there passed down only to be used 4 compoenents down
+    // TODO might want to use context for these since there passed down only to be used 4 compoenents down
     APIKeyProp: string;
     transcriptProp: string;
 }
@@ -35,7 +32,7 @@ interface Prompts {
 
 const SummaryPanel = (props: Props) => {
     const prompts: Prompts = promptsOBJ as Prompts;
-    const subjects = Object.keys(prompts).filter((key) => key !== "default"); //keep this line
+    const subjects = Object.keys(prompts).filter((key) => key !== "default");
     const promptTypes = Object.keys(prompts[subjects[0]].prompts);
     const [summary, setSummary] = React.useState(``);
     const [isLoading, setIsLoading] = React.useState(false);
